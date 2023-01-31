138a79961113a21278b447d808f2ed19bbdd82ac
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-01-31 14:20:49 +0630

    update route for UserController

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ Route::get('/', [BlogController::class,'index']);
 
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show'])->where('slug', '[A-z\d\-_]+');
 Route::get('/categories/{category:slug}', [CategoryController::class,'index'])->where('slug', '[A-z\d\-_]+');
-Route::get('/users/{user:id}', [UserController::class, 'index']);
+Route::get('/users/{user:name}', [UserController::class, 'index'])->where('name', '[A-z\d\-_]+');
 
