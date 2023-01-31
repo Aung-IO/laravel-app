88d1a72770e55bbedb98d5cb51305c1d00baa4b7
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-01-31 10:40:39 +0630

    create a relationship between blog and user

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,14 +9,14 @@ class Blog extends Model
 {
     use HasFactory;
 
-
+// a blog belongs to category
     public function category(){
 
         return $this->belongsTo(Category::class);
 
 
     }
-
+// a blog belongsto a user
     public function user()
     {
         return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::get('/', [BlogController::class,'index']);
 
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show'])->where('slug', '[A-z\d\-_]+');
 Route::get('/categories/{category:slug}', [CategoryController::class,'index'])->where('slug', '[A-z\d\-_]+');
+Route::get('/users/{user:id}', [UserController::class,'index'])->where('id', '[A-z\d\-_]+');
 
