9459f77c0a181f81380a0ff56cce215c93e19298
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-01-31 13:58:37 +0630

    add UserController route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,5 @@ Route::get('/', [BlogController::class,'index']);
 
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show'])->where('slug', '[A-z\d\-_]+');
 Route::get('/categories/{category:slug}', [CategoryController::class,'index'])->where('slug', '[A-z\d\-_]+');
-Route::get('/users/{user:id}', [UserController::class,'index'])->where('id', '[A-z\d\-_]+');
+Route::get('/users/{user:id}', [UserController::class, 'index']);
 
