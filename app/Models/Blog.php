ed428133fcdbe037ba04fea1001e075d84784d33
Aung Pyae <57700979+Aung-IO@users.noreply.github.com>
2023-02-03 17:03:41 +0630

    add scopeFilter

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,16 +9,56 @@ class Blog extends Model
 {
     use HasFactory;
 
-// a blog belongs to category
-    public function category(){
+    // a blog belongs to category
+    public function category()
+    {
 
         return $this->belongsTo(Category::class);
-
-
     }
-// a blog belongsto a user
+    // a blog belongsto a user
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter($query, $filter)
+    {
+        $query->when(
+            $filter['search'] ?? false,
+            function ($query, $search) {
+                $query = $query
+                    ->where(
+                        function ($query) use ($search) {
+                            $query->where('title', 'LIKE', '%' . $search . '%')
+                                ->orWhere('body', 'LIKE', '%' . $search . '%');
+                        }
+                    );
+            }
+
+        );
+        $query->when(
+            $filter['category'] ?? false,
+            function ($query, $slug) {
+                $query->whereHas(
+                    'category',
+                    function ($query) use ($slug) {
+                        $query->where('slug', $slug);
+                    }
+                );
+            }
+
+        );
+        $query->when(
+            $filter['user'] ?? false,
+            function ($query, $name) {
+                $query->whereHas(
+                    'user',
+                    function ($query) use ($name) {
+                        $query->where('name', $name);
+                    }
+                );
+            }
+
+        );
     }
 }
